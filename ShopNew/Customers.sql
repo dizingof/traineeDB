1badf97aafaefb23e705aeacacd038ca762b004f
Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-05-12 21:38:42 +0300

    add tables

---
+++ ShopNew/Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[CustomerID] NCHAR(10) NOT NULL PRIMARY KEY, 
+    [CompanyName] NVARCHAR(50) NOT NULL, 
+    [ContactName] NVARCHAR(50) NULL, 
+    [Phone] NVARCHAR(50) NULL
+)
---
+++ ShopNew/Orders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderID] INT NOT NULL PRIMARY KEY, 
+    [CustomerID] NCHAR(10) NOT NULL, 
+    [OrderDate] DATETIME NULL, 
+    [OrderQuantity] INT NULL, 
+    CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [Customers]([CustomerID])
+)
